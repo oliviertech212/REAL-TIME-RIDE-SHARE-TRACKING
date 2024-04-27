747929fcf78856a85d48579155ff2f4806a4fed4
oliviertech212 <oliviertech212@gmail.com>
2024-04-27 13:58:36 +0200

    get nearby transit station

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { useState, useEffect } from "react";
 function App() {
   const [currentLocation, setCurrentLocation] = useState(null);
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setCurrentLocation({
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
       });
-    });
+    }
   }, []);
   return (
     <div>
--- src/Map.jsx
+++ src/Map.jsx
@@ -6,10 +6,6 @@ import {
   Autocomplete,
   DirectionsRenderer,
 } from "@react-google-maps/api";
-const center = {
-  lat: -1.9578755,
-  lng: 30.11273499999993,
-};
 
 const Map = (props) => {
   const { isLoaded, loadError } = useJsApiLoader({
@@ -29,11 +25,10 @@ const Map = (props) => {
   const [directionsRes, setDirectionsRes] = React.useState(null);
   const [distance, setDistance] = React.useState(null);
   const [duration, setDuration] = React.useState(null);
-
   const originRef = React.useRef(null);
   const destinationRef = React.useRef(null);
 
-  const calculateRoute = () => {
+  const calculateRoute = async () => {
     if (originRef.current.value === "" && destinationRef.current.value === "") {
       return;
     }
@@ -45,11 +40,44 @@ const Map = (props) => {
         destination: destinationRef.current.value,
         travelMode: window.google.maps.TravelMode.DRIVING,
       },
-      (result, status) => {
+      async (result, status) => {
         if (status === window.google.maps.DirectionsStatus.OK) {
           setDirectionsRes(result);
           setDistance(result.routes[0].legs[0].distance.text);
           setDuration(result.routes[0].legs[0].duration.text);
+
+          // Assuming `map` is an instance of google.maps.Map
+          const placesService = new google.maps.places.PlacesService(map);
+
+          // For each waypoint in the route
+          directionsRes.routes[0].legs[0].steps.forEach((step) => {
+            // Search for bus stops near the waypoint
+            console.log("step", step.start_location);
+            const location = {
+              lat: step.start_location.lat(),
+              lng: step.start_location.lng(),
+            };
+            placesService.nearbySearch(
+              {
+                location: location,
+                radius: 200, // Search within a 500m radius
+                type: "transit_station",
+              },
+              (results, status) => {
+                console.log("results", results, status);
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                  // For each bus stop found
+                  results.forEach((place) => {
+                    // Create a marker on the map
+                    new google.maps.Marker({
+                      map,
+                      position: place.geometry.location,
+                    });
+                  });
+                }
+              }
+            );
+          });
         } else {
           console.error(`error fetching directions ${result}`);
         }
@@ -65,7 +93,7 @@ const Map = (props) => {
     destinationRef.current.value = "";
   };
 
-  // console.log("api key", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+  console.log("api key", process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
   if (loadError) {
     return <div>Error loading maps</div>;
   }
@@ -73,8 +101,18 @@ const Map = (props) => {
   if (!isLoaded) {
     return <div>Loading maps</div>;
   }
+  let center = {
+    lat: -1.9578755,
+    lng: 30.11273499999993,
+  };
 
-  console.log("Google Maps API loaded successfully", props.currentLocation);
+  if (props.currentLocation) {
+    center = {
+      lat: props.currentLocation.lat,
+      lng: props.currentLocation.lng,
+    };
+  }
+
   return (
     <>
       <div className=" relativew-[100vw] h-[100vh] bg-white">
@@ -88,7 +126,7 @@ const Map = (props) => {
               setMap(map);
             }}
           >
-            <Marker position={props.currentLocation} />
+            <Marker position={center} />
 
             {/* display direction responses */}
             {directionsRes && <DirectionsRenderer directions={directionsRes} />}
@@ -97,9 +135,9 @@ const Map = (props) => {
 
         <div
           className=" z-50 fixed  left-1/2 transform  right-1/2
-          w-1/2 border rounded-md h-52 m-auto flex flex-col items-center shadow-md bg-white"
+          w-1/2 border rounded-md m-auto flex flex-col items-center shadow-md bg-white"
         >
-          <div className="flex   m-auto  ">
+          <div className="flex   m-auto  mt-3 ">
             <Autocomplete>
               <input
                 placeholder="origin"
